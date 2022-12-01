f275d03aafc0e5f25c5052e29cdb4e5f183c08e7
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2022-12-01 07:20:32 +0100

    Added solution day 1

--- src/1/index.ts
+++ src/1/index.ts
@@ -1,10 +1,22 @@
-import { readLinesFromInput } from '../utils/readFile';
+import { readFileFromInput } from '../utils/readFile';
 
 const filename = './input/1.txt';
 
 export async function main() {
-  const input = await readLinesFromInput(filename);
+  const input = await readFileFromInput(filename);
+  const sumsOfCalories = input
+    .split('\r\n\r\n')
+    .map((calories) =>
+      calories
+        .split('\r\n')
+        .map((l) => parseInt(l))
+        .reduce((sum, current) => sum + current)
+    )
+    .sort((a, b) => b - a);
 
-  console.log('Part 1:');
-  console.log('Part 2:');
+  console.log('Part 1:', sumsOfCalories[0]);
+  console.log(
+    'Part 2:',
+    sumsOfCalories.slice(0, 3).reduce((sum, current) => sum + current)
+  );
 }
