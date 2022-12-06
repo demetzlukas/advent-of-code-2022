3058e4fd16bb1d26d912e37d8335d427b0bf4e66
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2022-12-06 06:39:36 +0100

    Added solution day 6

---
+++ src/6/index.ts
@@ -0,0 +1,27 @@
+import { readFileFromInput } from '../utils/readFile';
+
+const filename = './input/6.txt';
+
+export async function main() {
+  const input = await readFileFromInput(filename);
+  console.log('Part 1:', findFirstUniqueLetters(input, 4));
+  console.log('Part 2:', findFirstUniqueLetters(input, 14));
+}
+
+function findFirstUniqueLetters(
+  string: string,
+  numberOfUniqueLetters: number
+): number {
+  let chars = new Set(string.slice(0, numberOfUniqueLetters));
+  for (let index = numberOfUniqueLetters; index < string.length; index++) {
+    const element = string[index];
+
+    if (chars.size == numberOfUniqueLetters) {
+      return index - 1;
+    }
+
+    chars = new Set(string.slice(index - numberOfUniqueLetters, index));
+  }
+
+  return -1;
+}
